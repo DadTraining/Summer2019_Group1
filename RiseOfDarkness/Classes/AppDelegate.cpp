35877f95b3a827bd6febf2a163d5660a6fba739b
Nguyen Trong Hieu <tronghieu8531@gmail.com>
2019-07-08 17:20:07 +0700

    * Fix conflict

--- RiseOfDarkness/Classes/AppDelegate.cpp
+++ RiseOfDarkness/Classes/AppDelegate.cpp
@@ -24,7 +24,7 @@
 
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
-#include "PlayScene.h"
+#include "HomeScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 // #define USE_SIMPLE_AUDIO_ENGINE 1
@@ -100,7 +100,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = PlayScene::createScene();
+    auto scene = HomeScene::CreateScene();
 
     // run
     director->runWithScene(scene);
